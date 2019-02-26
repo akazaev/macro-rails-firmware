ca2b964656d1776259fda86745c13758cc51f7df
Aleksei Kazaev <kazaev.alex@gmail.com>
2019-02-26 21:37:26 +0300

    Initial commit

---
+++ stepper_full.py
@@ -0,0 +1,36 @@
+import OPi.GPIO as GPIO
+from time import sleep
+GPIO.setboard(GPIO.ZERO)
+GPIO.setmode(GPIO.BOARD)
+
+
+control_pins = (15, 19, 21, 23)
+
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
+
+halfstep_seq = [
+    [1, 0, 0, 1],
+    [1, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 1],
+]
+
+# halfstep_seq = list(reversed(halfstep_seq))
+
+# do full loop
+try:
+    for i in range(512):
+        for halfstep in range(len(halfstep_seq)):
+            for pin in range(4):
+                GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+            sleep(0.002)
+except KeyboardInterrupt:
+    pass
+except Exception as err:
+    print err
+
+
+for pin in control_pins:
+    GPIO.output(pin, 0)
