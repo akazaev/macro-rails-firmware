699f16ab327669b0e3f1aaf2316f1a00b281eb51
akazaev <kazaev.alex@gmail.com>
2020-04-06 20:33:18 +0300

    bugfixes

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 Auto-run Script Setup
 
-1. sudo nano /etc/profile
-2. Add:
+1. sudo raspi-config
+
+    Select “Boot Options” then “Desktop/CLI” then “Console Autologin”
+
+2. sudo nano /etc/profile
+3. Add:
 
     sudo /home/pi/macro-stacking-utility/power.py
     
     sudo /home/pi/macro-stacking-utility/run.py &
 
-3. sudo reboot
+4. sudo reboot
--- helpers.py
+++ helpers.py
@@ -6,8 +6,14 @@ _lcd = None
 def lcd_print(row1, row2):
     global _lcd
     if not _lcd:
-        _lcd = LCD(port=1)
+        try:
+            _lcd = LCD(port=1)
+        except Exception as err:
+            print(err)
     row1 = str(row1)
     row2 = str(row2)
-    _lcd.lcd_display_string(row1 + (16 - len(row1))*' ', 1)
-    _lcd.lcd_display_string(row2 + (16 - len(row2))*' ', 2)
+    try:
+        _lcd.lcd_display_string(row1 + (16 - len(row1))*' ', 1)
+        _lcd.lcd_display_string(row2 + (16 - len(row2))*' ', 2)
+    except Exception as err:
+        print(err)
